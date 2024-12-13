27ea4a52f950bc6435f0245ccfab41d04fc0468a
fadyh <fadyh@PC>
2024-12-13 02:13:21 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Octokit.Internal;
+using Refit;
+using System.Text.Json.Serialization;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void rollAnimal(object sender, EventArgs e)
+        {
+            string url = "https://some-random-api.com/animal/cat";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Fact newFact = JsonConvert.DeserializeObject<Fact>(json);
+
+                animalImage.Source = newFact.image;
+                factLabel.Text = newFact.fact;
+            }
+
+        }
+
+    }
+    internal class Fact
+    {
+        public string image { get; set; }
+        public string fact { get; set; }
+    }
+}
